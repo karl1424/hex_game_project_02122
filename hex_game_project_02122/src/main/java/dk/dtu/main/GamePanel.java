5ce339aacd3bd9351b034e6fe8da8120e5d0f9f9
karl1424 <155439737+karl1424@users.noreply.github.com>
2025-04-02 13:07:01 +0200

    Pop up with player 1 or player 2

--- hex_game_project_02122/src/main/java/dk/dtu/main/GamePanel.java
+++ hex_game_project_02122/src/main/java/dk/dtu/main/GamePanel.java
@@ -1,9 +1,11 @@
 package dk.dtu.main;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.Pane;
 
@@ -13,11 +15,36 @@ public class GamePanel extends Pane {
     private GameBoard gameBoard;
     private int gridSize;
     private ComputerOpponent computerOpponent;
+    private int computerPlayer;
 
     public GamePanel() {
         this.gridSize = 0;
-        boolean validInput = false;
 
+        chooseBoardSize();
+        choosePlayerNumber();
+        startGame();
+    }
+
+    private void choosePlayerNumber() {
+        List<String> choices = Arrays.asList("Player 1", "Player 2");
+
+        ChoiceDialog<String> dialog = new ChoiceDialog<>("Player 1", choices);
+        dialog.setTitle("Choose Player");
+        dialog.setHeaderText("HEX Game");
+        dialog.setContentText("Select your player:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        if (result.isPresent()) {
+            String selectedPlayer = result.get();
+            this.computerPlayer = selectedPlayer.equals("Player 1") ? 2 : 1;
+        } else {
+            System.exit(0);
+        }
+    }
+
+    private void chooseBoardSize() {
+        boolean validInput = false;
         while (!validInput) {
             TextInputDialog dialog = new TextInputDialog();
             dialog.setTitle("Input boardsize");
@@ -43,19 +70,18 @@ public class GamePanel extends Pane {
                 return;
             }
         }
-        startGame();
     }
 
     private void startGame() {
         gameBoard = new GameBoard(gridSize, gridSize);
         gui = new GUI(gridSize, gridSize, gameBoard, this);
-        computerOpponent = new ComputerOpponent(gameBoard, 1, gui);
+        computerOpponent = new ComputerOpponent(gameBoard, computerPlayer, gui);
         gui.setComputerOpponent(computerOpponent);
-        
+
         getChildren().add(gui);
         System.out.println("Initial empty board:");
         gameBoard.printBoard(gameBoard.board);
-        
+
         // Let computer go first if it's player 1
         if (computerOpponent.getPlayerNumber() == 1) {
             computerOpponent.makeMove();
@@ -70,13 +96,14 @@ public class GamePanel extends Pane {
         alert.setContentText(message);
         alert.showAndWait();
     }
-    
+
     // public void showGameOver(int winner) {
-    //     Alert alert = new Alert(AlertType.INFORMATION);
-    //     alert.setTitle("Game Over");
-    //     alert.setHeaderText("Game Finished");
-    //     alert.setContentText("Player " + winner + " (" + (winner == 1 ? "Red" : "Blue") + ") has won the game!");
-    //     alert.showAndWait();
+    // Alert alert = new Alert(AlertType.INFORMATION);
+    // alert.setTitle("Game Over");
+    // alert.setHeaderText("Game Finished");
+    // alert.setContentText("Player " + winner + " (" + (winner == 1 ? "Red" :
+    // "Blue") + ") has won the game!");
+    // alert.showAndWait();
     // }
 
     public boolean getTurn() {
