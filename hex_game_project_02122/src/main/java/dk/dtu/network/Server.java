777713588b6538b18b0163fa1ebc176d5e4f5aac
BubbiKingDK <Bubbi_King@Hotmail.com>
2025-06-12 04:23:39 +0200

    Godnat

--- hex_game_project_02122/src/main/java/dk/dtu/network/Server.java
+++ hex_game_project_02122/src/main/java/dk/dtu/network/Server.java
@@ -6,12 +6,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.jspace.*;
 
 import dk.dtu.network.handlers.ConnectionManager;
+import dk.dtu.network.handlers.LobbyHandler;
 import dk.dtu.network.tags.SpaceTag;
 import dk.dtu.network.tags.TupleTag;
 
-public class Server {
-    static Map<Integer, lobbyHandler> lobbyHandlers = new ConcurrentHashMap<>();
 
+public class Server {
+    public static Map<Integer, LobbyHandler> lobbyHandlers = new ConcurrentHashMap<>();
     public static SpaceRepository serverSpace;
     public static RemoteSpace lobbyRequests;
     public static int lobbyID;
@@ -46,9 +47,8 @@ public class Server {
 
     public static void createLobby() {
         try {
-            lobbyHandler tempLobbyHandler = new lobbyHandler(lobbyID, serverSpace);
+            LobbyHandler tempLobbyHandler = new LobbyHandler(lobbyID, serverSpace);
             lobbyHandlers.put(lobbyID, tempLobbyHandler); // Store the lobbys in a list - lobbyHandler handler
-                                                          // lobbyHandlers.get(1); //How to acces a specific
             new Thread(tempLobbyHandler).start();
             lobbyRequests.put(SpaceTag.LOBBY.value(), lobbyID);
             System.out.println("Lobby ID: " + lobbyHandlers.get(lobbyID).getLobbyId() + " have been created ");
@@ -57,100 +57,12 @@ public class Server {
             e.printStackTrace();
         }
     }
-}
 
-class lobbyHandler implements Runnable {
-    public int lobbyID;
-    private SpaceRepository serverSpace;
-    private RemoteSpace lobbySpace;
-    private boolean player2; // Used to check if the lobby is full
-    private volatile boolean running = true;
-
-    private ConnectionManager connectionManager;
-
-    public lobbyHandler(int lobbyID, SpaceRepository serverSpace) {
-        this.connectionManager = new ConnectionManager();
-        this.lobbyID = lobbyID;
-        this.serverSpace = serverSpace;
+    public static LobbyHandler getSpecificLobbyHandler(int lobbyID){
+        return lobbyHandlers.get(lobbyID);
     }
 
-    public void run() {
-        serverSpace.add(lobbyID + SpaceTag.LOBBY.value(), new SequentialSpace());
-        new Thread(() -> getConnect()).start();
-    }
-
-    public void getConnect() {
-        try {
-            lobbySpace = connectionManager.establishConnectionToRemoteSpace(lobbyID + SpaceTag.LOBBY.value());
-            connectionManager.performHandshakeServer(SpaceTag.LOBBY.value(), lobbySpace);
-            System.out.println("The host has joined Lobby: " + lobbyID);
-            new Thread(() -> checkCloseLobby()).start();
-            while (running) {
-                Object[] player2Status = lobbySpace.get(new ActualField(SpaceTag.LOBBY.value()), new FormalField(String.class));
-                if (player2Status[1].equals(TupleTag.TRYTOCONNECT.value())) {
-                    if (!checkOccupied(lobbySpace)) {
-                        System.out.println("Not occupied");
-                        lobbySpace.put(TupleTag.OCCUPIED.value());
-                        lobbySpace.put(TupleTag.CONNECTION.value(), TupleTag.CONNECTED.value());
-                        System.out.println("Player 2 has joined Lobby: " + lobbyID);                        
-                        lobbySpace.put(true); // Boolean to start
-                        player2 = true;
-                    } else {
-                        System.out.println("Occupied");
-                        lobbySpace.put(TupleTag.CONNECTION.value(), TupleTag.NOT_CONNECTED.value());
-                    }
-                } else {
-                    lobbySpace.get(new ActualField(TupleTag.OCCUPIED.value()));
-                    lobbySpace.put(false); // boolean not ready to start
-                    player2 = false;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void checkCloseLobby() {
-        try {
-            lobbySpace.get(new ActualField(TupleTag.CLOSE_LOBBY.value()));
-            lobbySpace.put(TupleTag.LOBBY_CLOSED.value());
-            long startTime = System.currentTimeMillis();
-            boolean acknowledged = false;
-            while (System.currentTimeMillis() - startTime < 2000) {
-                Object[] ack = lobbySpace.getp(new ActualField(TupleTag.ACKNOWLEDGE_CLOSE.value()));
-                if (ack != null) {
-                    acknowledged = true;
-                    break;
-                }
-                Thread.sleep(100);
-            }
-
-            if (!acknowledged) {
-                System.out.println("No acknowledge close received in 2 seconds, continuing anyway.");
-            }
-
-            serverSpace.remove(lobbyID + SpaceTag.LOBBY.value());
-            System.out.println("Lobby " + lobbyID + " closed and removed from repository");
-
-            Server.lobbyHandlers.remove(lobbyID);
-            running = false;
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean checkOccupied(Space space) {
-        Object[] isOccupied = null;
-        try {
-            isOccupied = space.queryp(new ActualField(TupleTag.OCCUPIED.value()));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return (isOccupied != null);
-    }
-
-    public int getLobbyId() {
-        return lobbyID;
+    public Map<Integer, LobbyHandler> getLobbyHandlers(){
+        return lobbyHandlers;
     }
 }
